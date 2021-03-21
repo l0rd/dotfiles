2e59152923cec9057d81d37253d06ee13d384800
Mario Loriedo <mario.loriedo@gmail.com>
2021-03-21 01:42:57 +0100

    First commit
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+├── `.aliases`      ---> [Mathias Bynens .aliases](https://github.com/mathiasbynens/dotfiles/blob/main/.aliases)  
+├── `LICENSE.txt`   ---> [The MIT License](https://opensource.org/licenses/MIT)
+└── `bootstrap.sh`  ---> [Mathias Bynens bootstrap.sh](https://github.com/mathiasbynens/dotfiles/blob/main/bootstrap.sh)  
---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE.txt" \
+		-avh --no-perms . ~;
+	source ~/.zshrc;
+}
+
+if [ "$1" = "--force" -o "$1" = "-f" ]; then
+	doIt;
+else
+	read -k "?This may overwrite existing files in your home directory. Are you sure? (y/n) ";
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
